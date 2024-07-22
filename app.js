ba45f62e00bd8844077d656dbd107d6fdc332f9c
Volodymyr I Pankiv <vpankiv@softserveinc.com>
2024-07-22 17:39:44 +0300

    topic-01

---
+++ app.js
@@ -0,0 +1,203 @@
+// alert("hello"); 
+// // comment here
+// console.log("Hello world!"); // console.log
+
+
+// console.log("Hello world!"); 
+
+
+// змінні
+// let const var
+
+// let message = "Hello World!";
+
+// message = "hello guys";
+
+// console.log(message);
+
+
+// const message = "hello guys";
+
+// message = "hello";
+
+// number
+// string
+// boolean
+// null
+// undefined
+// object
+
+// 24245325
+// "sfsfeg353535   "
+
+// true false
+
+// let message;
+
+// message = null;
+
+// console.log(message);
+
+// const user = {
+//     name: 'Volodymyr',
+//     age: 45,
+//     city: null
+// }
+
+// let number = 5;
+
+// number = "hello";
+
+// let number1 = 1;
+// let number2 = 2;
+
+// number1 = number2;
+
+// alert("hello");
+
+// alert(5);
+
+// let name = prompt("Whats your name?");
+
+// alert(name);
+// alert("Hello " + name);
+// alert(`Hello ${name}`);
+
+// + - * / %
+
+// console.log(5 + 5);
+
+// let number1 = 1;
+// let number2 = 2;
+
+// console.log(number1 + number2);
+// console.log(number1 - number2);
+
+// let number = 5;
+
+// number = number + 1;
+
+// number++;
+// number--;
+
+// number += 1; //6
+// number += 5; // 10
+// number -= 1
+// number *= 2;
+// number /= 2;
+
+// console.log(2 ** 4)
+
+
+
+// Math.pow(2, 4)
+// Math.sqrt(25);
+
+// console.log("5" + "1"); "51"
+
+// console.log("hello" + 'sfs');
+// console.log(5 - "1");
+
+
+// console.log(10 == 10);
+// console.log(10 == 11);
+// console.log(10 > 11);
+// console.log(10 < 11);
+
+// console.log(10 >= 10);
+// console.log(10 <= 10);
+
+
+
+
+
+// == - не строга рівність
+
+// console.log(10 == 10);
+// console.log(1 == true);
+// 0 - false
+// 1 - true
+// "" - false
+// "sfsf" - true
+
+// === - строга рівність !!!!
+
+// console.log(1 === true);
+// console.log(1 === 1);
+
+// let number = -1;
+
+
+// // console.log(number > 0 && number < 10);
+
+// console.log(number < 0 || number > 10);
+
+//  !
+// let x = true
+
+// x = !x;
+
+// const age = Number(prompt("How old are you?"));
+
+// let result = (age > 18) ? "you adult" : "you young";
+
+// console.log(result);
+
+// let number = 5;
+
+// console.log(typeof(number));
+
+// let message = "5";
+
+// let number = Number(message)
+
+// console.log(typeof(number));
+
+// if
+
+// switch
+
+// const age = Number(prompt("How old are you?"));
+
+// if (age > 18){
+//     console.log('you adult');
+// }
+
+// if (age > 18){
+//     console.log('you adult');
+// }else{
+//     console.log("u are too young");
+// }
+
+
+// if (age > 20 && age < 30){
+//     console.log("age is 20-30");
+// } else if(age > 30 && age < 40){
+//     console.log("age is 30-40");
+// }else if(age > 40 && age < 50){
+//     console.log("age is 40-50");
+// }else{
+//     console.log("out of range");
+// }
+
+
+// switch(age){
+//     case 20: // age === 30
+//         console.log("you are 20");
+//         break;
+//     case 30:
+//         console.log("you are 30");
+//         break;
+//     default:
+//         console.log("unknown age");
+//         break;
+// }
+
+// switch(true){
+//     case (age > 20 && age < 30): // true === true
+//         console.log("age is 20-30");
+//         break;
+//     case (age > 30 && age < 40):
+//         console.log("age is 30-40");
+//         break;
+// }
